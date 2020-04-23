631483dbb0fd449397701194507ca75542ea131f
Binyamin jan <64213925+binyamin786@users.noreply.github.com>
2020-04-24 01:28:55 +0430

    Update and rename zeroboy.py to noori.py

--- noori.py
+++ noori.py
@@ -47,19 +47,14 @@ def jalan(z):
 
 
 ##### LOGO #####
-logo = """ -----------------------------•◈•
-(  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
-|__~~~\ •◈•-----█-------⑦-------█------•◈•
-|__~~~\ •◈•-----█-------⑧-------█------•◈•
-|__~~~\ •◈•-----█-------⑥-------█------•◈•
+logo = """
+\033[1;91m▀█ █▀█ █▀█ █▄░█
+\033[1;91m█▄ █▄█ █▄█ █░▀█
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mBinyamin Jan
+\033[1;96mTelegram \033[1;93m: \033[1;KABIR_H4CK_001
+\033[1;96mFacebook  \033[1;93m: \033[1; binyamin786jan
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/binyamin786/jan
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +75,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mBinyamin
+\033[1;96mTelegram \033[1;93m: \033[1;92mKABIR_H4CK_001
+\033[1;96mFacebook  \033[1;93m: \033[1;92m binyamin786jan
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/binyamin786/jan
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "noori"
+CorrectPassword = "noori"
 
 loop = 'true'
 while (loop == 'true'):
